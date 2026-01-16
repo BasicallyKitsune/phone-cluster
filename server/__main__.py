741e58c4d6e71c04b9673d86cf48da261a683eca
Ari <basicallyahri@proton.me>
2026-01-16 00:54:05 -0500

    server: allow host and port via env vars

--- server/__main__.py
+++ server/__main__.py
@@ -4,11 +4,16 @@ phone-cluster: server.__main__
 Module entrypoint for running the Phone Cluster server.
 """
 
+import os
+
+host = os.getenv("PHONE_CLUSTER_HOST", "0.0.0.0")
+port = int(os.getenv("PHONE_CLUSTER_PORT", "8787"))
+
 def main():
     from server.app import create_app
 
     app = create_app()
-    app.run(host="0.0.0.0", port=8787)
+    app.run(host=host, port=port)
 
 
 if __name__ == "__main__":
